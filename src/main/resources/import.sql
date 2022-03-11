c391c9c57063aa46418d7fc6f9f5b35d2b54a24e
aagm404 <aagmerlini@gmail.com>
2022-03-11 00:28:54 -0300

    Mais ajustes de novo

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,6 +1,6 @@
-insert into tb_login (nome, digital) values ("Joao", "%Digital%Joao%");
-insert into tb_login (nome, digital) values ("Maria", "%Digital%Maria%");
-insert into tb_login (nome, digital) values ("Rodolfo", "%Digital%Rodolfo%");
-insert into tb_login (nome, digital) values ("Vilma", "%Digital%Vilma%");
-insert into tb_login (nome, digital) values ("Professo", "%Digital%Professor%");
+--insert into tb_login (nome, digital) values ("Joao", "%Digital%Joao%");
+--insert into tb_login (nome, digital) values ("Maria", "%Digital%Maria%");
+--insert into tb_login (nome, digital) values ("Rodolfo", "%Digital%Rodolfo%");
+--insert into tb_login (nome, digital) values ("Vilma", "%Digital%Vilma%");
+--insert into tb_login (nome, digital) values ("Professo", "%Digital%Professor%");
 
